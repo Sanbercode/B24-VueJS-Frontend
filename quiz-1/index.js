f040c7c55b902fed0192ad7c373bfbc91024710d
Rully Afrizal <74956021+rullyafrizal@users.noreply.github.com>
2021-05-01 19:35:15 +0700

    Quiz 1 : Function Penghasil Tanggal Hari Esok

--- quiz-1/index.js
+++ quiz-1/index.js
@@ -10,3 +10,28 @@ var kalimat_2 = "Saya Iqbal"
 jumlah_kata(kalimat_1); // 6
 jumlah_kata(kalimat_2); // 2
 
+
+/* Soal 1 : Function Penghasil Tanggal Hari Esok */
+var next_date = function (tanggal , bulan , tahun ) {
+    var months = [
+        "Januari", "Februari", "Maret",
+        "April", "Mei", "Juni",
+        "Juli", "Agustus", "September",
+        "Oktober", "November", "Desember"
+    ];
+    var d = new Date();
+    d.setDate(tanggal);
+    d.setMonth(bulan - 1);
+    d.setFullYear(tahun);
+
+    d.setDate(d.getDate() + 1); // mengeset hari besok
+    var formatDate = d.getDate() + " " + months[d.getMonth()] + " " + d.getFullYear();
+
+    console.log(formatDate);
+}
+
+var tanggal = 31
+var bulan = 12
+var tahun = 2020
+
+next_date(tanggal , bulan , tahun ) // output : 1 Januari 2021
