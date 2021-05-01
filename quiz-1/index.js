d43bfdbd4b50506e65b3d055eaf03a2a1fb8f818
Rully Afrizal <74956021+rullyafrizal@users.noreply.github.com>
2021-05-01 19:41:19 +0700

    Quiz 1 : Function Penghitung Jumlah Kata

--- quiz-1/index.js
+++ quiz-1/index.js
@@ -22,3 +22,17 @@ var bulan = 12
 var tahun = 2020
 
 next_date(tanggal , bulan , tahun ) // output : 1 Januari 2021
+
+
+
+/* Soal 2 : Penghitung Jumlah Kata */
+var jumlah_kata = function (kalimat) {
+    var arrKalimat = kalimat.trim().split(" ");
+    console.log(arrKalimat.length);
+}
+
+var kalimat_1 = " Halo nama saya Muhammad Iqbal Mubarok"
+var kalimat_2 = "Saya Iqbal"
+
+jumlah_kata(kalimat_1); // 6
+jumlah_kata(kalimat_2); // 2
