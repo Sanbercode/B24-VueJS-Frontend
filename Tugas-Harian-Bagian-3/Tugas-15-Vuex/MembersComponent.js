d727abebd8f6fc6a10c0912b82d6a95c6a668178
Rully Afrizal <74956021+rullyafrizal@users.noreply.github.com>
2021-05-24 00:27:35 +0700

    Selesai : Tugas-15-Vuex

---
+++ Tugas-Harian-Bagian-3/Tugas-15-Vuex/MembersComponent.js
@@ -0,0 +1,124 @@
+export const MembersComponent = {
+    props: ['members', 'showedMember'],
+    template: `
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">No.</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Address</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Photo Profile</th>
+                    <th scope="col">
+                        Action
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(member, index) in members" :key="member.id">
+                    <td>{{ ++index }}</td>
+                    <td>{{ member.name }}</td>
+                    <td>{{ member.address }}</td>
+                    <td>{{ member.no_hp }}</td>
+                    <td><img :src="member.photo_profile ? 'http://demo-api-vue.sanbercloud.com/' + member.photo_profile : 'https://dummyimage.com/16:9x1080'"
+                            alt="avatar" width="200px"></td>
+                    <td>
+                        <button class="btn btn-sm btn-warning" @click="editMember(member)"><i class="fa fa-pencil"></i></button>
+                        <button class="btn btn-sm btn-danger" @click="deleteMember(member.id)"><i class="fa fa-trash"></i></button>
+                        <button type="button" class="btn btn-sm btn-success my-1" @click="showMember(member.id)"
+                                data-toggle="modal" data-target="#uploadPhotoProfile"><i class="fa fa-upload"></i></button>
+                        <button type="button" class="btn btn-sm btn-info" @click="showMember(member.id)" data-toggle="modal"
+                                data-target="#detailMember"><i class="fa fa-eye"></i></button>
+                        <div class="modal fade" id="detailMember" data-backdrop="static" data-keyboard="false" tabindex="-1"
+                             aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="staticBackdropLabel">Details Member</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <img :src="showedMember.photo_profile ? 'http://demo-api-vue.sanbercloud.com/' + showedMember.photo_profile : 'https://dummyimage.com/16:9x1080'" class="rounded mx-auto d-block" alt="avatar" width="450px">
+                                        <table class="table my-3">
+                                            <thead class="thead-dark">
+                                            <tr>
+                                                <th scope="col">Name</th>
+                                                <th scope="col">Address</th>
+                                                <th scope="col">Phone</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            <tr>
+                                                <td>{{ showedMember.name }}</td>
+                                                <td>{{ showedMember.address }}</td>
+                                                <td>{{ showedMember.no_hp }}</td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+        
+        
+                        <div class="modal fade" id="uploadPhotoProfile" data-backdrop="static" data-keyboard="false"
+                             tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="uploadPhoto">Upload Photo Profile</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="custom-file my-3">
+                                            <input type="file" id="myfile" ref="file" name="foto" class="custom-file-input"
+                                                   onchange="$(this).next().after().text($(this).val().split('\\\\').slice(-1)[0])"
+                                                   @change="previewFiles"
+                                                   accept="image/*, application/json">
+                                            <label class="custom-file-label" for="myfile">Choose Photo Profile...</label>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary" @click.prevent="uploadPhoto(showedMember.id, files)"
+                                                data-dismiss="modal">Submit
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    `,
+    methods: {
+        previewFiles() {
+            this.files = this.$refs.file[0].files[0];
+        },
+        async showMember (id) {
+            await this.$store.commit('showMember', id);
+        },
+        async deleteMember (id) {
+            await this.$store.commit('deleteMember', id);
+        },
+        async uploadPhoto (id, files) {
+            await this.$store.commit('uploadPhoto', {id, files});
+        },
+        editMember (member) {
+            this.$store.commit("editMember", member);
+        }
+    },
+    data () {
+        return {
+            files:[]
+        }
+    }
+}
